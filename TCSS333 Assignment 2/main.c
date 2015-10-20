0c492877d1c358da33d74836712e8f4b56260bc9
Alexander Terikov <teraliv@gmail.com>
2015-10-19 21:35:13 -0700

    Project is done

--- TCSS333 Assignment 2/main.c
+++ TCSS333 Assignment 2/main.c
@@ -1,177 +1,127 @@
-#include <stdio.h>
-#include <stdbool.h>
+/*
+ * TCSS 333 - Autumn 2015
+ *
+ * Assignment 2 - Bitmap image manipulation.
+ * Alex Terikov
+ */
 
-void checkboard(void);
+#include <stdio.h>
+
+void imageDoubleExposure(unsigned char *image1, unsigned char *image2);
+void imageCheckerBoard(unsigned char *image1, unsigned char *image2);
 
 int main(void) {
     
-    unsigned char in1Arr[499446];
-    unsigned char in2Arr[499446];
+    // arrays to hold image data
+    unsigned char image1[499446];
+    unsigned char image2[499446];
     
-//    unsigned char header1[14];
-//    unsigned char header2[40];
+    // read the bitmap image files in binary mode
+    FILE *bmpFile1 = fopen("in1.bmp", "rb");
+    FILE *bmpFile2 = fopen("in2.bmp", "rb");
     
-    unsigned char arr2[499446];
-    unsigned char checkboardArr[499446];
-    
-    int i;
-    
-    int row;   // Row number, from 0 to 7
-    int col;   // Column number, from 0 to 7
-    int count = 54;
-    bool status = false;
-    
-//    checkboard();
-    
-    // read the file in binary mode - rb
-    FILE *in1 = fopen("in1.bmp", "rb");
-    FILE *in2 = fopen("in2.bmp", "rb");
-    FILE *outFile = fopen("doubleExposure.bmp", "wb");
-    FILE *outFile2 = fopen("checkboard.bmp", "wb");
-    
-    // check if the file is open
-    if (!in1 || !in2)
-    {
+    // check if the files are open
+    if (!bmpFile1 || !bmpFile2) {
         perror("Failed to open file");
     }
     
+    // load image data into arrays
+    fread(image1, 1, 499446, bmpFile1);
+    fread(image2, 1, 499446, bmpFile2);
     
-    fread(in1Arr, 1, 499446, in1);
-    fread(in2Arr, 1, 499446, in2);
+    // make a double exposure image
+    imageDoubleExposure(image1, image2);
+    // make a checker board image
+    imageCheckerBoard(image1, image2);
     
-    
-    // copy header 1
-//    for (i = 0; i < 14; i++) {
-//        header1[i] = arr[i];
-//    }
-
-    // copy header 2
-//    for (i = 14; i < 54; i++) {
-//        header2[i] = arr[i];
-//    }
-    
-    
-//    for (i = 0; i < 14; i++) {
-//        arr2[i] = header1[i];
-//    }
-//    
-//    for (i = 14; i < 54; i++) {
-//        arr2[i] = header2[i];
-//    }
-    
-    // header
-    for (i = 0; i < 54; i++) {
-        arr2[i] = in1Arr[i];
-    }
-    
-    
-    // double exposure
-    for (i = 54; i < 499446; i++) {
-        arr2[i] = (in1Arr[i] + in2Arr[i]) / 2;
-    }
-    
-//    printf("%d\n", arr2[55]);
-//    arr2[55] = arr2[55] - 50;
-//    printf("%d\n", arr2[55]);
-    
-    // checkboard
-    for (i = 0; i < 54; i++) {
-        checkboardArr[i] = in1Arr[i];
-    }
-    
-    for ( row = 0;  row < 8;  row++ ) {
-        
-        for ( col = 0;  col < 408;  col++) {
-
-            if ( (row % 2) == (col % 2) )
-                // printf("*");
-                status = true;
-            else
-                // printf("-");
-                status = false;
-            
-            
-            
-            if (status) {
-                i = 0;
-                while (i < (153)) {
-//                    printf("*");
-//                    printf("%d ", count++);
-                    checkboardArr[count] = in1Arr[count];
-                    count++;
-                    i++;
-                }
-            } else {
-                i = 0;
-                while (i < (153)) {
-//                    printf("-");
-//                    printf("%d ", count++);
-                    checkboardArr[count] = in2Arr[count];
-                    count++;
-                    i++;
-                }
-            }
-        }
-//        printf("\n");
-        
-    } // end for row
-    
-    printf("%d\n", count);
-    
-    // end of checkboard
-    
-    
-    
-    fwrite(arr2, sizeof(char), 499446, outFile);
-    fwrite(checkboardArr, sizeof(char), 499446, outFile2);
-    
-    fclose(in1);
-    fclose(in2);
-    fclose(outFile);
-    fclose(outFile2);
+    // close files
+    fclose(bmpFile1);
+    fclose(bmpFile2);
     
     return 0;
 }
 
-void checkboard(void) {
-    int row;   // Row number, from 0 to 7
-    int col;   // Column number, from 0 to 7
+/*
+ * Creates a bitmap image file with double exposure
+ * effect base on two images.
+ *
+ * @param *image1 - pointer to the 1st image array
+ * @param *image2 - pointer to the 2nd image array
+ */
+void imageDoubleExposure(unsigned char *image1, unsigned char *image2) {
+    
+    // image file to write
+    FILE *bmpFile = fopen("blend.bmp", "wb");
+    
     int i;
-    int num = 100;
+    unsigned char doubleExposure[499446]; // array to hold image data
+
+    // load header data into array
+    for (i = 0; i < 54; i++) {
+        doubleExposure[i] = image1[i];
+    }
     
-    bool status = false;
-//    int x,y;   // Top-left corner of square
+    // load double exposure data into array
+    for (i = 54; i < 499446; i++) {
+        doubleExposure[i] = (image1[i] + image2[i]) / 2;
+    }
     
+    // wride bmp image data into file
+    fwrite(doubleExposure, sizeof(unsigned char), 499446, bmpFile);
+    fclose(bmpFile); // close file
+}
+
+
+/*
+ * Creates a bitmap image file with checker board
+ * effect base on two images.
+ *
+ * @param *image1 - pointer to the 1st image array
+ * @param *image2 - pointer to the 2nd image array
+ */
+void imageCheckerBoard(unsigned char *image1, unsigned char *image2) {
+    
+    // image file to write
+    FILE *bmpFile = fopen("checker.bmp", "wb");
+    
+    int i;
+    unsigned char checkerBoard[499446]; // array to hold image data
+    
+    int row;   // Row number
+    int col;   // Column number
+    int count = 54; // Current pixel color location
+    
+    // load header data into array
+    for (i = 0; i < 54; i++) {
+        checkerBoard[i] = image1[i];
+    }
+    
+    // load data from two image arrays as a check board
     for ( row = 0;  row < 8;  row++ ) {
         
-        for ( col = 0;  col < 8;  col++) {
-//            x = col * 20;
-//            y = row * 20;
-            if ( (row % 2) == (col % 2) )
-                // printf("*");
-                status = true;
-            else
-                // printf("-");
-                status = false;
+        for ( col = 0;  col < 408;  col++) {
             
-            
-            i = 0;
-            
-            if (status) {
-                while (i < 5) {
-                    printf("*");
-//                    printf("%d ", ++num);
-                    i++;
-                }
-            } else {
-                while (i < 5) {
-                    printf("-");
-//                    printf("%d ", ++num);
+            if ( (row % 2) == (col % 2) ) {
+                i = 0;
+                while (i < (153)) {
+                    checkerBoard[count] = image1[count];
+                    count++;
                     i++;
                 }
             }
-        }
-        printf("\n");
-        
-    } // end for row
+            else {
+                i = 0;
+                while (i < (153)) {
+                    checkerBoard[count] = image2[count];
+                    count++;
+                    i++;
+                }
+            }
+
+        } // end for cols
+    } // end for rows
+    
+    // wride bmp image data into file
+    fwrite(checkerBoard, sizeof(unsigned char), 499446, bmpFile);
+    fclose(bmpFile); //close file
 }
